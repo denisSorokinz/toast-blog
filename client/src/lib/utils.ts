83097f0dce61df82cfc33c8dba77d9b146e09702
denisSorokinZ <d.sorokin@gmail.com>
2024-02-24 01:38:09 +0200

    feat(client/): debounced autosave w/ optimistic updates

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -28,3 +28,50 @@ export function debounce(fn: (...args: any[]) => any, delay = 300): any {
     }, delay);
   };
 }
+
+/*
+TASK:
+develop debouncePromise/debounceRequest/debounceFetcher+ function
+that takes in a url/fetcher+ fn that returns Promise and:
+~ resolves if immediate call or timeout and no further calls
+~ rejects if timeout hasn't passed, and new fn call has been made
+*/
+
+export function debounceFetcher(
+  fetcher: (...args: any[]) => Promise<any>,
+  delay = 300,
+) {
+  let timeout: NodeJS.Timeout | undefined;
+  let callCbOnTimeoutEnd = false;
+  let didCallOnTimeoutEnd = false;
+
+  let controller = new AbortController();
+
+  let prevReject: Function | null = null;
+
+  return (...args: any[]) =>
+    new Promise((res, rej): any => {
+      callCbOnTimeoutEnd = !!timeout || didCallOnTimeoutEnd;
+      if (!timeout && !didCallOnTimeoutEnd) {
+        fetcher(...args, controller.signal).then((data) => res(data));
+      } else {
+        controller.abort();
+        controller = new AbortController();
+        prevReject && prevReject();
+      }
+
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        timeout = undefined;
+        if (callCbOnTimeoutEnd)
+          fetcher(...args, controller.signal).then((data) => res(data));
+
+        didCallOnTimeoutEnd = true;
+        setTimeout(() => {
+          didCallOnTimeoutEnd = false;
+        }, delay);
+      }, delay);
+
+      prevReject = rej;
+    });
+}
